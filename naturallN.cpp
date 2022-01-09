821c30e10c9a29a979fc7893ef84c16caa48fcce
Taneesha Singh <95531688+Taneesha-02@users.noreply.github.com>
2022-01-09 22:16:36 +0530

    Add files via upload

---
+++ naturallN.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=0;
+    int n;
+    cin>>n;
+    while(i<=n)
+    {
+        cout<<i++<<endl;
+    }
+    return 0;
+}
+    
+    
+    
+    
+    
+    
+    
---
+++ reverseN.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    int n;
+    cout<<"enter value of n";
+    cin>>n;
+    i=n;
+    while(i>0)
+    {
+        cout<<i--<<endl;
+    }
+return 0;
+}
+    
+    
+    
+    
+    
+    
+    
