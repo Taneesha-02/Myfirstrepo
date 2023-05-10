019c82dbf4a4f9659ec692f07efd0daf09030dbe
Taneesha Singh <95531688+Taneesha-02@users.noreply.github.com>
2023-05-10 22:34:12 +0530

    Add files via upload

---
+++ FirstnlastOccurence.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int firstOcc ( int arr[], int n , int key) {
+    int s = 0 ,e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while (s<=e){
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid -1;
+        }
+        else if ( key > arr[mid]){
+            s = mid + 1;
+        }
+        else if ( key < arr[mid]){
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main(){
+    int arr[6] = {1,2,3,3,5};
+    cout<< " First Occurence of 3 is " << firstOcc(arr,6,3)<<endl;
+    return 0;
+}
